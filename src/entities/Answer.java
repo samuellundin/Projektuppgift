93a3c5392f2b63618aa1aa805acc3155a7a63c96
drybone <jackie_chan1994@hotmail.com>
2017-05-04 13:59:41 +0200

    Entity for Answer

---
+++ src/entities/Answer.java
@@ -0,0 +1,36 @@
+package entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by Leon on 2017-05-04.
+ */
+
+@Entity
+public class Answer {
+
+    @Id
+    @GeneratedValue( strategy= GenerationType.AUTO )
+
+    private int answerId;
+    private String answerText;
+
+    public int getAnswerId() {
+        return  answerId;
+    }
+
+    public void setAnswerId(int answerId) {
+        this.answerId = answerId;
+    }
+
+    public String getAnswerText() {
+        return answerText;
+    }
+
+    public void setAnswerText(String answerText) {
+        this.answerText = answerText;
+    }
+}
