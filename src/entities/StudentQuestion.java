e87f7659bc2012618ec0280827833405260d7d0e
drybone <jackie_chan1994@hotmail.com>
2017-05-04 14:26:52 +0200

    Entities for studentTest and studentQuestion

---
+++ src/entities/StudentQuestion.java
@@ -0,0 +1,64 @@
+package entities;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * Created by Leon on 2017-05-04.
+ */
+
+@Entity
+public class StudentQuestion {
+
+    @Id
+    @GeneratedValue( strategy=GenerationType.AUTO )
+
+    private int questionId;
+    private String studentAnswer;
+    private int studentTestId;
+
+    @OneToMany(targetEntity = StudentTest.class)
+    private List studentTestList;
+
+    public StudentQuestion() {
+
+    }
+
+    public StudentQuestion(int questionId, String studentAnswer, int studentTestId) {
+        this.questionId = questionId;
+        this.studentAnswer = studentAnswer;
+        this.studentTestId = studentTestId;
+    }
+
+    public int getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(int questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getStudentAnswer() {
+        return studentAnswer;
+    }
+
+    public void setStudentAnswer(String studentAnswer) {
+        this.studentAnswer = studentAnswer;
+    }
+
+    public int getStudentTestId() {
+        return studentTestId;
+    }
+
+    public void setStudentTestId(int studentTestId) {
+        this.studentTestId = studentTestId;
+    }
+
+    public List getStudentTestList() {
+        return studentTestList;
+    }
+
+    public void setStudentTestList(List studentTestList) {
+        this.studentTestList = studentTestList;
+    }
+}
