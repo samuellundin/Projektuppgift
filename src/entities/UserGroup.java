fc394cba49eaf8fbb6ed9ae031d11bd3f9a784b2
josef <joseflundstrom@gmail.com>
2017-05-09 14:40:58 +0200

    Skapat UserGroup entitet, som är en lista över users.

--- src/entities/UserGroup.java
+++ src/entities/UserGroup.java
@@ -15,44 +15,13 @@ public class UserGroup implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int userId;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String password;
-    private int role;
+
 
     public void UserGroup() {
 
         ArrayList userList = new ArrayList();
 
-        userList.add(1,firstName);
-        userList.add(2,lastName);
-        userList.add(3,email);
-        userList.add(4,password);
-        userList.add(5,role);
-    }
+      //  userList.add(1,firstName);
 
-    public int getUserId() {
-        return userId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public int getRole() {
-        return role;
     }
 }
