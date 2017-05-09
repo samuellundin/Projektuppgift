7c4094927f7e13021591afcb13f3eefa32387ce8
samuellundin <samuel.lundin21@gmail.com>
2017-05-09 10:36:40 +0200

    Startpage

--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -2,6 +2,40 @@ package controllers;
 
 public class Controller {
 
+    public void addTestAction() {
+    }
 
+    public void editTestAction() {
+    }
+
+    public void gradeTestAction() {
+    }
+
+    public void shareTestAction() {
+    }
+
+    public void addUserAction() {
+    }
+
+    public void editUserAction() {
+    }
+
+    public void addGroupAction() {
+    }
+
+    public void editGroupAction() {
+    }
+
+    public void takeTestAction() {
+    }
+
+    public void showResultAction() {
+    }
+
+    public void settingsAction() {
+    }
+
+    public void logoutAction() {
+    }
 
 }
