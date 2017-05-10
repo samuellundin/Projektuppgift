fa3283c71d2c3d5de01e2352f9528b4d8e411290
josef <joseflundstrom@gmail.com>
2017-05-10 09:19:34 +0200

    Skapat en arrayList som lagrar data från User som en array.

--- src/entities/UserGroup.java
+++ src/entities/UserGroup.java
@@ -3,6 +3,7 @@ package entities;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by josef on 5/9/2017.
@@ -16,10 +17,12 @@ public class UserGroup implements Serializable{
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int userId;
 
-
     public void UserGroup() {
 
+        User u = new User();
+
         ArrayList userList = new ArrayList();
+        userList.addAll(Arrays.asList(u));
 
       //  userList.add(1,firstName);
 
